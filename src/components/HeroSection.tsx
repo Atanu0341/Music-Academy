07c2f367b8e19241403a392d8fd17cd67bc3f4b0
Atanu Majumder <atanumajumder2004@gmail.com>
2024-03-11 09:33:46 +0530

    Refactor HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,6 @@ import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
 import { Button } from "./ui/moving-border"
 
-
 function HeroSection() {
     return (
         <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
@@ -11,8 +10,12 @@ function HeroSection() {
                 fill="white"
             />
             <div className="p-4 relative z-10 w-full text-center">
-                <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of music</h1>
-                <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+                <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+                    Master the art of music
+                </h1>
+                <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+                    Dive into our comprehensive music courses and transform your musical journey today. Whether you&apos;re a beginner or looking to refine your skills, join us to unlock your true potential.
+                </p>
                 <div className="mt-4">
                     <Link href={"/courses"}>
                         <Button borderRadius="1.75rem" className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800">
@@ -25,4 +28,4 @@ function HeroSection() {
     )
 }
 
-export default HeroSection
+export default HeroSection;
